d223bf23cdc91b41c87412f2feeeaeb79601b56a
Ryan Richardson <ryananthonyrichardson@gmail.com>
2014-06-08 20:07:30 -0700

    Create /new page form and set some typography

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var path = require('path');
 var lessMiddleware = require('less-middleware');
+var shortId = require('shortid');
 var app = express();
+
+var siteTitle = "FreeDraftBoard.com - Free offline drafting for fantasy sports leagues."
+
+var prependToTitle = function(title) {
+  return title + " | " + siteTitle;
+}
+
+shortId.seed(461);
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
@@ -16,15 +25,24 @@ app.use(lessMiddleware(path.join(__dirname, 'less'), {
 }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.render('layout');
+app.get('/', function(req, res){ 
+  res.render('index', {pageTitle: siteTitle}); 
 });
 
-app.get('/board', function(req, res){
-  res.render('board');
+app.get('/new', function(req, res){
+  console.log(req);
+  res.render('new', {
+    url_id: shortId.generate(),
+    pageTitle: prependToTitle("Board Creator")
+  }); 
+});
+
+app.get('/board', function(req, res){ 
+  res.render('board', {pageTitle: prependToTitle("Live Draft Board")});
 });
 
 app.get('/*', function(req, res){
+  res.status(404);
   res.send('404 Not Found');
 });
 
---
+++ public/js/board_settings.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  $("#teams_setting").change(function() {
+
+    var teams_showing = $(".team_submission").size();
+
+
+    if ($(this).val() !== "") {
+
+      $("#team_names").show();
+
+      if (teams_showing < $(this).val()) {
+        for (i=teams_showing; i<$(this).val(); i++) {
+          $("#submit_settings").show();
+          $("#team_names").append('<input class="team_submission" id="team_' + i + '" type="text">')
+        }
+      } else if (teams_showing > $(this).val()) {
+        for (i=teams_showing; i>=$(this).val(); i--) {
+          $("#team_" + i).remove();
+        }
+      }
+
+    } else {
+      $("#team_names").empty().hide();
+      $("#submit_settings").hide();
+    }
+
+  });
+
+});
