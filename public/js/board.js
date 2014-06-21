b8ae986c88cf6fe716d518bb9483589799bdea61
Ryan Richardson <ryananthonyrichardson@gmail.com>
2014-06-21 16:34:41 -0700

    Fix timeout for better perfomance when resizing

--- public/js/board.js
+++ public/js/board.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
-
-  function scaleBoard(boardWidth) {
-    while (boardWidth % settings.teams.length !== 0) { boardWidth--; }
-    var unitWidth = (boardWidth / settings.teams.length);
-    var pixelFraction = 1 / settings.teams.length;
-    setTimeout(function() {
-      $(".league, .round").css({width: boardWidth + "px"});
-      $(".pick").css({width: (unitWidth - pixelFraction) + "px", height: (unitWidth - pixelFraction) + "px"});
-      $(".team").css({width: (unitWidth - pixelFraction) + "px"});
-    }, 100);
-  }
   
   function init(settings) {
 
+    function scaleBoard(boardWidth) {
+      console.log(shouldResize);
+      if (shouldResize) {
+        while (boardWidth % settings.teams.length !== 0) { boardWidth--; }
+        var unitWidth = (boardWidth / settings.teams.length);
+        var pixelFraction = 1 / settings.teams.length;
+        $(".league, .round").css({width: boardWidth + "px"});
+        $(".pick").css({width: (unitWidth - pixelFraction) + "px", height: (unitWidth - pixelFraction) + "px"});
+        $(".team").css({width: (unitWidth - pixelFraction) + "px"});
+        shouldResize = false;
+      }
+      setTimeout(function() {
+        shouldResize = true;
+      }, 500);
+    }
+
     console.log(settings.minutes);
     console.log(settings.seconds);
+
+    var shouldResize = false;
 
     var tileWidth = Math.floor(100.0 / settings.teams.length);
     console.log(tileWidth);
@@ -22,14 +29,6 @@ $(document).ready(function() {
     var $teams = $(".team");
     var $picks = $(".pick");
     var $tiles = $(".team, .pick");
-
-    // if (settings.serpentine) {
-    //   var $evenRounds = $(".round.even");
-    //   $evenRounds.each(function() {
-    //     var evenPicks = $(this).children();
-    //     $(this).append(evenPicks.get().reverse());
-    //   });
-    // }
 
     $tiles.each(function(){
       $(this).css({width: tileWidth + "%"});
