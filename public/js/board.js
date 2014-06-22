abb0d52789d6b2aaee41aedb2b0b173d96492ade
Ryan Richardson <ryananthonyrichardson@gmail.com>
2014-06-22 16:22:34 -0700

    Skip scaling timeout on initial render

--- public/js/board.js
+++ public/js/board.js
@@ -3,28 +3,30 @@ $(document).ready(function() {
   function init(settings) {
 
     function scaleBoard(boardWidth) {
-      console.log(shouldResize);
-      if (shouldResize) {
+      if (initialRender || shouldResize) {
         while (boardWidth % settings.teams.length !== 0) { boardWidth--; }
         var unitWidth = (boardWidth / settings.teams.length);
         var pixelFraction = 1 / settings.teams.length;
         $(".league, .round").css({width: boardWidth + "px"});
-        $(".pick").css({width: (unitWidth - pixelFraction) + "px", height: (unitWidth - pixelFraction) + "px"});
+        $(".pick").css({width: (unitWidth - pixelFraction) + "px"});
+        var actualWidth = $(".pick").first().css('width');
+        $(".pick").css({height: actualWidth});
         $(".team").css({width: (unitWidth - pixelFraction) + "px"});
         shouldResize = false;
+        initialRender = false;
       }
       setTimeout(function() {
         shouldResize = true;
-      }, 500);
+      }, 100);
     }
 
-    console.log(settings.minutes);
-    console.log(settings.seconds);
+    // console.log(settings.minutes);
+    // console.log(settings.seconds);
 
     var shouldResize = false;
+    var initialRender = true; // to skip timeout logic on initial board scaling
 
     var tileWidth = Math.floor(100.0 / settings.teams.length);
-    console.log(tileWidth);
 
     var $teams = $(".team");
     var $picks = $(".pick");
